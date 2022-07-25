1894a6fe3fa213d2063d9945f741d266d7cf50da
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2022-07-25 10:07:09 -0300

    Install tailwind

--- pragstudio_liveview_code/config/config.exs
+++ pragstudio_liveview_code/config/config.exs
@@ -13,7 +13,11 @@ config :pragstudio_liveview_code,
 # Configures the endpoint
 config :pragstudio_liveview_code, PragstudioLiveviewCodeWeb.Endpoint,
   url: [host: "localhost"],
-  render_errors: [view: PragstudioLiveviewCodeWeb.ErrorView, accepts: ~w(html json), layout: false],
+  render_errors: [
+    view: PragstudioLiveviewCodeWeb.ErrorView,
+    accepts: ~w(html json),
+    layout: false
+  ],
   pubsub_server: PragstudioLiveviewCode.PubSub,
   live_view: [signing_salt: "pDHHhD2q"]
 
@@ -46,6 +50,17 @@ config :logger, :console,
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
+
+config :tailwind,
+  version: "3.1.6",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- pragstudio_liveview_code/config/dev.exs
+++ pragstudio_liveview_code/config/dev.exs
@@ -26,7 +26,8 @@ config :pragstudio_liveview_code, PragstudioLiveviewCodeWeb.Endpoint,
   secret_key_base: "nTJuWfTmB9tvBkTJiuXV0XPJJ8zpghvzy5DowviUCEBEX6PC3OCVhZACp98jD+Pg",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
