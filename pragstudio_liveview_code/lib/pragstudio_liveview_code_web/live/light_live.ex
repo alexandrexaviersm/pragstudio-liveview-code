7acaf37c3dffebee26920ac55d8b77934c6d1832
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2022-07-30 18:13:36 -0300

    1 - Add radio button to LightLive

--- pragstudio_liveview_code/lib/pragstudio_liveview_code_web/live/light_live.ex
+++ pragstudio_liveview_code/lib/pragstudio_liveview_code_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule PragstudioLiveviewCodeWeb.LightLive do
   use PragstudioLiveviewCodeWeb, :live_view
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, :brightness, 10)
+    socket = assign(socket, brightness: 10, temp: 3000)
     {:ok, socket}
   end
 
@@ -11,7 +11,8 @@ defmodule PragstudioLiveviewCodeWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}>
+        <span style={"background-color: #{temp_color(@temp)};
+                      width: #{@brightness}%"}>
           <%= @brightness %>%
         </span>
       </div>
@@ -52,9 +53,24 @@ defmodule PragstudioLiveviewCodeWeb.LightLive do
       <div class="amount">
         <%= @brightness %>
       </div>
+
+      <form phx-change="change-temp">
+        <input type="radio" id="3000" name="temp" value="3000" checked={3000 == @temp} />
+        <label for="3000">3000</label>
+
+        <input type="radio" id="4000" name="temp" value="4000" checked={4000 == @temp} />
+        <label for="4000">4000</label>
+
+        <input type="radio" id="5000" name="temp" value="5000" checked={5000 == @temp} />
+        <label for="5000">5000</label>
+    </form>
     </div>
     """
   end
+
+  defp temp_color(3000), do: "#F1C40D"
+  defp temp_color(4000), do: "#FEFF66"
+  defp temp_color(5000), do: "#99CCFF"
 
   def handle_event("on", _, socket) do
     socket = assign(socket, :brightness, 100)
@@ -84,6 +100,12 @@ defmodule PragstudioLiveviewCodeWeb.LightLive do
   def handle_event("update", %{"brightness" => brightness}, socket) do
     brightness = String.to_integer(brightness)
     socket = assign(socket, :brightness, brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event("change-temp", %{"temp" => temp}, socket) do
+    temp = String.to_integer(temp)
+    socket = assign(socket, :temp, temp)
     {:noreply, socket}
   end
 end
