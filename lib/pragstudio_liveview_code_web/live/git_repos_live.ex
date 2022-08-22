9b4d4d7c2e3ae6da3cdcd538a1fcb426d56f31c8
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2022-08-21 23:45:20 -0300

    GitRepos - Create filter form

--- lib/pragstudio_liveview_code_web/live/git_repos_live.ex
+++ lib/pragstudio_liveview_code_web/live/git_repos_live.ex
@@ -4,7 +4,7 @@ defmodule PragstudioLiveviewCodeWeb.GitReposLive do
   alias PragstudioLiveviewCode.GitRepos
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, repos: GitRepos.list_git_repos())
+    socket = assign(socket, repos: GitRepos.list_git_repos(), language: "")
     {:ok, socket}
   end
 
@@ -12,47 +12,70 @@ defmodule PragstudioLiveviewCodeWeb.GitReposLive do
     ~H"""
     <h1>Trending Git Repos</h1>
     <div id="repos">
-      <div class="repos">
-        <ul>
-          <%= for repo <- @repos do %>
-            <li>
-              <div class="first-line">
-                <div class="group">
-                  <img src="images/terminal.svg">
-                  <a href={repo.owner_url}>
-                    <%= repo.owner_login %>
-                  </a>
-                  /
-                  <a href={repo.url}>
-                    <%= repo.name %>
-                  </a>
+      <form phx-change="filter">
+        <select name="language">
+          <%= options_for_select(language_options(), @language) %>
+        </select>
+      </form>
+      <div id="repos">
+        <div class="repos">
+          <ul>
+            <%= for repo <- @repos do %>
+              <li>
+                <div class="first-line">
+                  <div class="group">
+                    <img src="images/terminal.svg">
+                    <a href={repo.owner_url}>
+                      <%= repo.owner_login %>
+                    </a>
+                    /
+                    <a href={repo.url}>
+                      <%= repo.name %>
+                    </a>
+                  </div>
+                  <button>
+                    <img src="images/star.svg">
+                    Star
+                  </button>
                 </div>
-                <button>
-                  <img src="images/star.svg">
-                  Star
-                </button>
-              </div>
-              <div class="second-line">
-                <div class="group">
-                  <span class={"language #{repo.language}"} >
-                    <%= repo.language %>
-                  </span>
-                  <span class="license">
-                    <%= repo.license %>
-                  </span>
-                  <%= if repo.fork do %>
-                    <img src="images/fork.svg">
-                  <% end %>
+                <div class="second-line">
+                  <div class="group">
+                    <span class={"language #{repo.language}"} >
+                      <%= repo.language %>
+                    </span>
+                    <span class="license">
+                      <%= repo.license %>
+                    </span>
+                    <%= if repo.fork do %>
+                      <img src="images/fork.svg">
+                    <% end %>
+                  </div>
+                  <div class="stars">
+                    <%= repo.stars %> stars
+                  </div>
                 </div>
-                <div class="stars">
-                  <%= repo.stars %> stars
-                </div>
-              </div>
-            </li>
-          <% end %>
-        </ul>
+              </li>
+            <% end %>
+          </ul>
+        </div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("filter", %{"language" => language}, socket) do
+    socket =
+      assign(socket, repos: GitRepos.list_git_repos(language: language), language: language)
+
+    {:noreply, socket}
+  end
+
+  defp language_options do
+    [
+      "All Languages": "",
+      "Elixir": "elixir",
+      Ruby: "ruby",
+      JavaScript: "javascript"
+    ]
   end
 end
